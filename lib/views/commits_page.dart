29748312b17606c0b9be7406207705f10476d886
Vivek Kumar Gupta <vivekkumargupta680@gmail.com>
2020-01-06 22:48:02 +0530

    Fetch Data from api and display commit

--- lib/views/commits_page.dart
+++ lib/views/commits_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class CommitsPage extends StatefulWidget {
   final String url;
@@ -7,9 +10,52 @@ class CommitsPage extends StatefulWidget {
 }
 
 class CommitsPageState extends State<CommitsPage> {
+  String user;
+  String repo;
+  dynamic commits;
+  @override
+  void initState() {
+    var s = widget.url.split('/');
+    user = s[s.length-2];
+    repo = s[s.length-1];
+    _fetchdata();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(repo+' <'+user+'>'),
+      ),
+      body: commits == null
+            ? Container()
+            : showCommits(),
+    );
+  }
+
+  _fetchdata() async {
+    String url = 'https://api.github.com/repos/'+user+'/'+repo+'/commits';
+    var response = await http.get(url);
+    setState(() {
+      commits = jsonDecode(response.body);
+      print(commits[0]);
+    });
+  }
+
+  showCommits() {
+    return ListView.builder(
+      itemCount: commits.length,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: Text(commits[position]["commit"]["committer"]["date"]),
+            title: Text(commits[position]["commit"]["message"]),
+          ),
+        );
+      },
+    );
   }
 
 }
