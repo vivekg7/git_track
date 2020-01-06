1778a3cb50c1647295f4b868eb501f60adfb2df1
Vivek Kumar Gupta <vivekkumargupta680@gmail.com>
2020-01-06 15:23:07 +0530

    Created Home Screen

---
+++ lib/views/commits_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CommitsPage extends StatefulWidget {
+  final String url;
+  CommitsPage(this.url);
+  CommitsPageState createState() => CommitsPageState();
+}
+
+class CommitsPageState extends State<CommitsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
---
+++ lib/views/home_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:git_track/utils/navigation.dart';
+
+import 'commits_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+
+  TextEditingController controller = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Git Track"),
+      ),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: TextField(
+                autofocus: false,
+                controller: controller,
+                keyboardType: TextInputType.url,
+                decoration: InputDecoration(
+                  labelText: 'repo url',
+                  hintText: 'https://github.com/vivekg7/git_track',
+                  border: OutlineInputBorder(),
+                ),
+                style: TextStyle(),
+                textAlign: TextAlign.center,
+                onSubmitted: (val) {
+                  _onSubmit();
+                },
+              ),
+            ),
+            RaisedButton(
+              padding: EdgeInsets.only(left: 30.0, right: 30.0),
+              child: Text("Submit"),
+              onPressed: () {
+                _onSubmit();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _onSubmit() {
+    //NavigationUtil.showBlockingDialog(context);
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => CommitsPage(controller.text)));
+  }
+
+}
